5170dcbe9ebc26298f02700c3bc10fc6507772d9
Evan Allrich <evan@unguku.com>
2014-04-04 00:02:06 -0500

    Stat prefixes and some always-on counters

--- core/defaults.py
+++ core/defaults.py
@@ -1,4 +1,18 @@
+backends = (
+    'backends.stdout',
+)
+
+
 flush_interval = 10000 # milliseconds
+
+
+# stat names
+# ----------
+prefix_stats = "statsd"
+bad_lines_seen          = prefix_stats + ".bad_lines_seen"
+packets_received        = prefix_stats + ".packets_received"
+timestamp_lag_namespace = prefix_stats + ".timestamp_lag"
+
 
 logging = {
     'format':  '%(asctime)s | [%(levelname)s] %(message)s',
