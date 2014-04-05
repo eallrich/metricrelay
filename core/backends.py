3bb315b30a197c1e33413ca343cc7effc4921575
Evan Allrich <evan@unguku.com>
2014-04-05 13:22:21 -0500

    Don't need settings

--- core/backends.py
+++ core/backends.py
@@ -1,4 +1,7 @@
-from . import defaults
+"""Simple backend thread.
+
+Calls each backend's flush function with the processed metrics data
+"""
 
 def flush(backends, data):
     for backend in backends:
